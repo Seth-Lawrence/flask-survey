666fe2bfd9190992fa247144c23fe807ed7c1633
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-30 12:31:51 -0800

    added question endpoint and html

--- app.py
+++ app.py
@@ -11,9 +11,31 @@ debug = DebugToolbarExtension(app)
 responses = []
 
 @app.get('/')
-def show_survey():
+def show_survey_start():
+    '''show initial instructions and button to start survey'''
 
-    return render_template("survey_start.html")
+    return render_template(
+        "survey_start.html",
+        title=survey.title,
+        instructions=survey.instructions
+        )
+
+@app.post('/begin')
+def begin_survey():
+    '''post for when user clicks on button to start'''
+
+    return redirect('/questions/0')
+
+@app.get('/questions/<int:question_number>')
+def show_question(question_number):
+    '''show question for survey, input question number to show'''
+
+    question = survey.questions[question_number]
+
+    return render_template(
+        'question.html',
+        question=question
+    )
 
 
 
