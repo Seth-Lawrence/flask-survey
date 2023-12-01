d8cc0fd03f1e7fa5c55ed5eb117929168c5a2fb5
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-30 17:21:17 -0800

    protected questions for step 7

--- app.py
+++ app.py
@@ -27,12 +27,20 @@ def begin_survey():
     '''post for when user clicks on button to start'''
 
     session["responses"] = []
+    session['question_number'] = 0
+    session['survey_complete'] = False
 
     return redirect('/questions/0')
 
 @app.get('/questions/<int:question_number>')
 def show_question(question_number):
     '''show question for survey, input question number to show'''
+
+    if session['survey_complete']:
+        return redirect('/thank-you')
+
+    if question_number != session['question_number']:
+        return redirect(f"/questions/{session['question_number']}")
 
     question = survey.questions[question_number]
 
@@ -51,6 +59,7 @@ def handle_answer(question_number):
     session["responses"] = responses
 
     next_question = question_number + 1
+    session['question_number'] += 1
 
     if next_question < len(survey.questions):
         return redirect(f'/questions/{next_question}')
@@ -61,6 +70,9 @@ def handle_answer(question_number):
 @app.get('/thank-you')
 def survey_completion():
     '''showing thank-you screen with thank you results'''
+
+    session['survey_complete'] = True
+
     return render_template(
         "completion.html",
         questions=survey.questions,
