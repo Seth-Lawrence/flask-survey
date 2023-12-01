54c95ae04387acb4d5e67b15f056b7cca7510c4a
Seth Hobson <sethlawrencehobson@gmail.com>
2023-11-30 19:36:41 -0500

    test personality_quiz

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
+# from surveys import personality_quiz as survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
